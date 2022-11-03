8022c9e73ee434c6a5f564ba34b3f3092d7eae5c
mehmetbatar35 <mehmetbatar120@gmail.com>
2022-11-03 20:33:35 +0100

    Add project files.

---
+++ Musteri.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.Ders
+{
+    internal class Musteri
+    {
+
+        public int Id { get; set; }
+        public string MusteriNo { get; set; }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+
+
+
+
+
+using _05.Ders;
+
+GercekMusteri musteri1 = new GercekMusteri();
+musteri1.Id = 1;
+musteri1.MusteriNo = "12345";
+musteri1.Adi = "Ali";
+musteri1.soyadi = "Can";
+musteri1.TcNo = "7563201485";
+
+TuzelMusteri musteri2 = new TuzelMusteri();
+musteri2.Id = 2;
+musteri2.MusteriNo = "54321";
+musteri2.SirketAdi = "Gurcan Otomotiv";
+musteri2.VergiNo = "1299900000";
+
+
+Musteri musteri3 = new GercekMusteri();
+Musteri musteri4 = new TuzelMusteri();
+
+
+
+CustomerManager customerManager = new CustomerManager();
+
+customerManager.Add(musteri1);
+customerManager.Add(musteri2);
+customerManager.Add(musteri3);
+customerManager.Add(musteri4);
